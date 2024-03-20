6cee2026b57c13406c1678841838577ad285072c
ANTOINE BERTHELOT <ANTOINE.BERTHELOT65@GMAIL.COM>
2024-03-20 11:01:44 +0100

    Update script.js / style.css  card Restaurant

--- script.js
+++ script.js
@@ -42,9 +42,12 @@ function createRegion(regions) {
 }
 
 createRegion(regionArray);
+
+/* Tableau des cartes des restaurants différents A.B */
+
 const restaurantArray = [
   {
-    img: "",
+    img: "./ressources/images/city.jpg",
     type: "Ville",
     name: "Le carbet d'Oc",
     adresse: "8 rue des filatiers 31 100 Toulouse",
@@ -54,7 +57,7 @@ const restaurantArray = [
     accessibilité: "Parking à proximité, Terrasse,",
   },
   {
-    img: "",
+    img: "./ressources/images/campagne.jpg",
     type: "Campagne",
     name: " La terrasse du TNG",
     adresse: "Allée du lac, 32600 l'Isle Jourdain",
@@ -64,57 +67,95 @@ const restaurantArray = [
     accessibilité: "Parking à proximité, Terrasse",
   },
   {
-    img: "",
+    img: "./ressources/images/coupdecoeur.jpg",
     type: "Coup de Coeur",
     name: "Chez Loulou",
     adresse: "Col des Palomières, 65200 Gerde",
     description:
       "Attention, établissement mythique s'il en est ! L'ambiance compte ici autant que les plats. Il faut vivre ces repas où seuls les saisons et les produits forment la carte. Elle n'est pas très chargée, du bon, du très bon et du local : confits, faux-filet, garbure, charcuteries, poulet à l'écrevisse, fromage du pays, tarte normande aux fruits rouges, vin... C'est copieux, c'est bon, le cadre est champêtre, l'accueil est décontracté et les prix sont doux, que demander de plus !",
-    site: "Pas de site",
+    site: "pas de site internet",
     accessibilité:
       "Route de montagne, Restaurant sur la gauche en arrivant en haut du Col.",
   },
 ];
 
+/* Programmation du changement de carte de restaurant (sélection) A.B */
+/* tableau : changer les valeurs de chacun ?? */
 const cardDescriptionRestaurant = document.querySelector(
   ".card-restaurant-button"
 );
 
 function createCardRestaurant(selectKeys) {
   selectKeys.forEach((selectKey) => {
-    const cardCityRestaurant = document.createElement("h2");
-    cardcityRestaurant.classList.add("city-restaurant");
-    cardCityRestaurant.textContent = selectKey.type;
-    descriptionRestaurant.appendChild(cardCityRestaurant);
+    /* image de ma card restaurant */
+
+    const imgCardContainerRestaurant = document.createElement("div");
+    imgCardContainerRestaurant.classList.add("img-container-card-restaurant");
+    cardRegion.appendChild(imgCardContainerRestaurant);
+
+    const imageCardrestaurant = document.createElement("img");
+    imageCardrestaurant.classList.add("img-card-restaurant");
+    imageCardrestaurant.src = selectKey.img;
+    imgCardContainerRestaurant.appendChild(imageCardrestaurant);
+
+    /* contenu de ma card restaurant descriptif */
+
+    const cardRestaurantDescription = document.createElement("div");
+    cardRestaurantDescription.classList.add("card-restaurant-description");
+    cardRegion.appendChild(cardRestaurantDescription);
+
+    /* title sont parent de la div du contour */
+
+    const titleCityRestaurant = document.createElement("h2");
+    titleCityRestaurant.classList.add("title-city-restaurant");
+    titleCityRestaurant.textContent = selectKey.type;
+    cardRestaurantDescription.appendChild(titleCityRestaurant);
+
+    /* nom du restaurant  */
 
     const nameRestaurant = document.createElement("h3");
     nameRestaurant.classList.add("name-restaurant");
     nameRestaurant.textContent = selectKey.name;
-    cardCityRestaurant.appendChild(nameRestaurant);
+    cardRestaurantDescription.appendChild(nameRestaurant);
+
+    /* adresse */
 
     const adresseRestaurant = document.createElement("h4");
     adresseRestaurant.classList.add("adresse-restaurant");
     adresseRestaurant.textContent = selectKey.adresse;
-    nameRestaurant.appendChild(adresseRestaurant);
+    cardRestaurantDescription.appendChild(adresseRestaurant);
+
+    /* description */
 
     const descriptionRestaurant = document.createElement("p");
     descriptionRestaurant.classList.add("description-restaurant");
     descriptionRestaurant.textContent = selectKey.description;
-    adresseRestaurant.appendChild(descriptionRestaurant);
+    cardRestaurantDescription.appendChild(descriptionRestaurant);
+
+    /* site internet */
 
     const siteRestaurant = document.createElement("p");
     siteRestaurant.classList.add("site-restaurant");
     siteRestaurant.textContent = selectKey.site;
-    descriptionRestaurant.appendChild(siteRestaurant);
+    cardRestaurantDescription.appendChild(siteRestaurant);
+
+    /* accessibilité */
 
     const accessibiliteRestaurant = document.createElement("p");
     accessibiliteRestaurant.classList.add("accessibilite-restaurant");
     accessibiliteRestaurant.testContent = selectKey.accessibilité;
-    siteRestaurant.appendChild(accessibiliteRestaurant);
+    cardRestaurantDescription.appendChild(accessibiliteRestaurant);
+
+    /* bouton parent de card région */
 
     const buttonCardRestaurant = document.createElement("div");
     buttonCardRestaurant.classList.add("button-card-restaurant");
-    accessibiliteRestaurant.appendChild(buttonCardRestaurant);
+    cardRegion.appendChild(buttonCardRestaurant);
+
+    const buttonCardRestaurantStyle = document.createElement("button");
+    buttonCardRestaurantStyle.classList.add("second-button-card-restaurant");
+    buttonCardRestaurant.appendChild(buttonCardRestaurant);
   });
 }
 cardDescriptionRestaurant.addEventListener("click");
+createCardRestaurant(restaurantArray);
